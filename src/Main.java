d4960697f1ec5803d9bfcd050089c0f014643735
Nik <nikocoolboy@yandex.ru>
2023-06-22 16:52:54 +0300

    исправление по замечаниям

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,68 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Main result = new Main();
+        System.out.print("Введите строку с двумя целыми операндами от 1 до 10 включительно и один оператор (+, -, /, *: ");
+        String str = input.nextLine();
+        String answer = result.calc(str);
+        System.out.println("Output:\n" + answer);
+        input.close();
+    }
+    public static String calc(String input) {
         String myStr1 = "";
         String myStr2 = "";
+        String outstr = "формат математической операции не удовлетворяет заданию - два целых операнда от 1 до 10 включительно и один оператор (+, -, /, *";
         int j;
-        int k=1;
+        int k = 1;
         double ans;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите строку с двумя операндами от 1 до 10 включительно и один оператор (+, -, /, *: ");
-        String str = scanner.nextLine();
-        char[] charArray = str.toCharArray();
-
-        for (int i=0; i<charArray.length; i++) {
-            if (Character.isDigit(charArray[i])){
-                myStr1 = myStr1+Character.toString(charArray[i]);}
-            else {break;}
-            k = k+1;};
-        for (j=k; j<charArray.length; j++) {
-            if (Character.isDigit(charArray[j])) {
-                myStr2 = myStr2 + Character.toString(charArray[j]);}
+        char[] charArray = input.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (Character.isDigit(charArray[i])) {
+                myStr1 = myStr1 + Character.toString(charArray[i]);
+            } else {
+                break;
+            }
+            k = k + 1;
         }
-            if ("".equalsIgnoreCase(myStr1)) {
-            System.out.println("формат математической операции не удовлетворяет заданию - два операнда от 1 до 10 включительно и один оператор (+, -, /, *");
+        for (j = k; j < charArray.length; j++) {
+            if (Character.isDigit(charArray[j])) {
+                myStr2 = myStr2 + Character.toString(charArray[j]);
+            }
+        }
+        if ("".equalsIgnoreCase(myStr1)) {
+            System.out.println(outstr);
             System.exit(0);
         }
         if ("".equalsIgnoreCase(myStr2)) {
-            System.out.println("формат математической операции не удовлетворяет заданию - два операнда от 1 до 10 включительно и один оператор (+, -, /, *");
+            System.out.println(outstr);
             System.exit(0);
         }
-        if (Double.parseDouble(myStr1)>10){
-            System.out.println("формат математической операции не удовлетворяет заданию - два операнда от 1 до 10 включительно и один оператор (+, -, /, *");
+        if (Double.parseDouble(myStr1) > 10 || Double.parseDouble(myStr1) < 1) {
+            System.out.println(outstr);
             System.exit(0);
         }
-        if (Double.parseDouble(myStr2)>10) {
-            System.out.println("формат математической операции не удовлетворяет заданию - два операнда от 1 до 10 включительно и один оператор (+, -, /, *");
+        if (Double.parseDouble(myStr2) > 10 || Double.parseDouble(myStr2) < 1) {
+            System.out.println(outstr);
             System.exit(0);
         }
-            switch(charArray[k-1]) {
-            case '+': ans = (int)(Double.parseDouble(myStr1) + Double.parseDouble(myStr2));
+        switch (charArray[k - 1]) {
+            case '+':
+                ans = (int) (Double.parseDouble(myStr1) + Double.parseDouble(myStr2));
                 break;
-            case '-': ans = (int)(Double.parseDouble(myStr1) - Double.parseDouble(myStr2));
+            case '-':
+                ans = (int) (Double.parseDouble(myStr1) - Double.parseDouble(myStr2));
                 break;
-            case '*': ans = (int)(Double.parseDouble(myStr1) * Double.parseDouble(myStr2));
+            case '*':
+                ans = (int) (Double.parseDouble(myStr1) * Double.parseDouble(myStr2));
                 break;
-            case '/': ans = (int)(Double.parseDouble(myStr1) / Double.parseDouble(myStr2));
+            case '/':
+                ans = (int) (Double.parseDouble(myStr1) / Double.parseDouble(myStr2));
                 break;
-            default:  System.out.printf("строка не является математической операцией");
-                return;
+            default:
+                return ("строка не является математической операцией");
+
         }
-        System.out.printf(Double.parseDouble(myStr1) + " " + charArray[k-1] + " " + Double.parseDouble(myStr2) + " = " + ans);
-        scanner.close();
+        return (Double.parseDouble(myStr1) + " " + charArray[k - 1] + " " + Double.parseDouble(myStr2) + " = " + ans);
     }
 }
